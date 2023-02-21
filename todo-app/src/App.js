f4c27580dc4b858fc7aaff61b3ec671313edac3c
Fikayo Jetawo <jetawof@gmail.com>
2023-02-21 23:54:04 +0100

    running npm install

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
 import "./App.css";
-import AddTodo  from "./Addtodo";
+import AddTodo from "./Addtodo";
 
 class App extends Component {
   state = {
     todos: [
-      {id: 1, content: "Buy some milk"},
-      {id: 2, content: "Buy some Biscuit"}
+      { id: 1, content: "Buy some milk" },
+      { id: 2, content: "Buy some Biscuit" }
     ]
   }
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id
     });
-    this.setState ({
+    this.setState({
       todos
     })
   }
   addTodo = (todo) => {
-      todo.id = Math.random();
-      let todos = [...this.state.todos, todo]
-      this.setState({
-        todos
-      })
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
   }
   render() {
-  return (
-    <div className="App container">
+    return (
+      <div className="App container">
         <h1 className="text-center text-primary display-1">Todo's</h1>
-        <AddTodo addTodo={this.addTodo}/> <br/>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>       
-    </div>
-  );
-}
+        <AddTodo addTodo={this.addTodo} /> <br />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
